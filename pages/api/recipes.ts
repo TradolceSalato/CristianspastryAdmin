4693ba511399ee2ed21382205aa5311fa0fa4e16
Cristian Sorrentino <tradolcesalatoinfo@gmail.com>
2023-01-16 19:15:25 +0100

    modificato file recipes.ts (json)

--- pages/api/recipes.ts
+++ pages/api/recipes.ts
@@ -6,7 +6,7 @@ const Recipes = [
         TitoloRicetta : "Torta Della Nonna",
         ImmagineRicetta : "https://www.tavolartegusto.it/wp/wp-content/uploads/2018/03/Torta-della-nonna-Ricetta-Torta-della-nonna-.jpg",
         Ingredienti : ["100gr di zucchero","40gr di farina","2n uova"],
-        TempoDiPreparazione : 20,
+        TempoDiPreparazione : "20",
         Passaggi : ["1 : mischiare ","2 : boh"],
         Note : ""
        },
@@ -14,7 +14,7 @@ const Recipes = [
         TitoloRicetta : "Macaron",
         ImmagineRicetta : "https://sweetmoment.altervista.org/wp-content/uploads/2019/04/macarons-perfetti1.jpg",
         Ingredienti : [],
-        TempoDiPreparazione : 20,
+        TempoDiPreparazione : "20",
         Passaggi : [],
         Note : ""
        },
@@ -22,7 +22,7 @@ const Recipes = [
         TitoloRicetta: "Sbrisolona",
         ImmagineRicetta: "https://www.cucchiaio.it/content/cucchiaio/it/ricette/2009/12/ricetta-torta-sbrisolona/_jcr_content/header-par/image_single.img.jpg/1614349780378.jpg",
         Ingredienti : [],
-        TempoDiPreparazione : 20,
+        TempoDiPreparazione : "20",
         Passaggi : [],
         Note : ""
        },
@@ -30,7 +30,7 @@ const Recipes = [
         TitoloRicett : "Biscotti",
         ImmagineRicetta : "https://www.ricettedellanonna.net/wp-content/uploads/2022/01/biscotti-al-burro-9-620x414.jpg",
         Ingredienti : [],
-        TempoDiPreparazione : 20,
+        TempoDiPreparazione : "20",
         Passaggi : [],
         Note : ""
        },
@@ -38,7 +38,7 @@ const Recipes = [
        TitoloRicetta : "Pasta Frolla",
        ImmagineRicetta : "https://www.giallozafferano.it/images/173-17338/Pasta-frolla_450x300.jpg",
        Ingredienti : [],
-       TempoDiPreparazione : 20,
+       TempoDiPreparazione : "20",
        Passaggi : [],
        Note : ""
    },
@@ -46,7 +46,7 @@ const Recipes = [
        TitoloRicetta : "Bigne",
        ImmagineRicetta : "https://www.pianetadessert.it/wordpress/wp-content/uploads/2016/08/image-25.jpeg",
        Ingredienti : [],
-       TempoDiPreparazione : 20,
+       TempoDiPreparazione : "20",
        Passaggi : [],
        Note : ""
    },
@@ -54,7 +54,7 @@ const Recipes = [
        TitoloRicetta: "Crema Pasticcera",
        ImmagineRicetta : "https://www.pianetadessert.it/wp-content/uploads/2016/06/image-1024x838.jpeg",
        Ingredienti : [],
-       TempoDiPreparazione : 20,
+       TempoDiPreparazione : "20",
        Passaggi : [],
        Note : ""
    },
@@ -62,7 +62,7 @@ const Recipes = [
        TitoloRicetta : "Meringhe",
        ImmagineRicetta : "https://www.pianetadessert.it/wordpress/wp-content/uploads/2015/05/image.jpeg",
        Ingredienti : [],
-       TempoDiPreparazione : 20,
+       TempoDiPreparazione : "20",
        Passaggi : [],
        Note : ""
    },
@@ -70,7 +70,7 @@ const Recipes = [
        TitoloRicetta : "Delizia Al Limone",
        ImmagineRicetta : "https://www.giallozafferano.it/images/178-17863/Delizie-al-limone_450x300.jpg",
        Ingredienti : [],
-       TempoDiPreparazione : 20,
+       TempoDiPreparazione : "20",
        Passaggi : [],
        Note : ""
    },
@@ -78,7 +78,7 @@ const Recipes = [
        TitoloRicetta : "Pan DI Limone",
        ImmagineRicetta : "https://www.giallozafferano.it/images/176-17653/Pan-di-limone_780x520_wm.jpg",
        Ingredienti : [],
-       TempoDiPreparazione : 20,
+       TempoDiPreparazione : "20",
        Passaggi : [],
        Note : ""
    },
@@ -86,7 +86,7 @@ const Recipes = [
        TitoloRicetta : "Pan DI Limoneee2",
        ImmagineRicetta : "https://www.giallozafferano.it/images/176-17653/Pan-di-limone_780x520_wm.jpg",
        Ingredienti : [],
-       TempoDiPreparazione : 20,
+       TempoDiPreparazione : "20",
        Passaggi : [],
        Note : ""
    }
